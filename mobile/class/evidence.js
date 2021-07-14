d0ae7c1bd55d3a68b9ee3c5457cb5749a4bbdbf2
dawidbros1 <dawidbros1@wp.pl>
2021-07-14 12:07:43 +0200

    Pierwsza wersja programu dostosowana pod urządzenia mobilne

---
+++ mobile/class/evidence.js
@@ -0,0 +1,6 @@
+class Evidence {
+    constructor(index, name) {
+        this.index = index;
+        this.name = name;
+    }
+}
---
+++ mobile/class/ghost.js
@@ -0,0 +1,21 @@
+class Ghost {
+    constructor() {
+        // Empty
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    setPower(power) {
+        this.power = power;
+    }
+
+    setWeakness(weakness) {
+        this.weakness = weakness;
+    }
+
+    setEvidence(evidence) {
+        this.evidence = evidence;
+    }
+}
