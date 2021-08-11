1848a59e4294c47aefbdd26744b977651774e395
dawidbros1 <dawidbros1@wp.pl>
2021-08-11 18:18:52 +0200

    css + images

--- desktop/src/functions.js
+++ desktop/src/functions.js
@@ -1,11 +1,3 @@
-function zerosArray(array) {
-    for (var i = 0; i < array.length; i++) {
-        array[i] = 0;
-    }
-
-    return array;
-}
-
 function removeA(arr) {
     var what, a = arguments, L = a.length, ax;
     while (L > 1 && arr.length) {
