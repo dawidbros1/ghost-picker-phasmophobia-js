d79e457826f5949648bb94efab3fcd8d3ff8be81
dawidbros1 <dawidbros1@wp.pl>
2021-12-09 09:33:49 +0100

    Nowe teksty

--- src/Database/feedbackDatabase.js
+++ src/Database/feedbackDatabase.js
@@ -10,12 +10,13 @@ class FeedbackDatabase {
             'Przepadnij w czeluści piekieł duchu ',
             'Tak jak pan Jezus powiedział, przepadnij duchu nieczysty imieniem ',
             'Dwieście złotych się należy, duchem, który nawiedzał Pańską posesję jest ',
-            'Jak z fakturą to trzysta złotych, duchem, który nawiedzał Pańską posesję jest '
+            'Jak z fakturą to trzysta złotych się należy, duchem, który nawiedzał Pańską posesję jest ',
+            'Jak z fakturą to będzie trzy stówki proszę Pana, duch to  '
         ];
     }
 
     random() {
         let x = Math.ceil(Math.random() * 100); // 0-100
-        return this.feedback[x % this.feedback.length]  
+        return this.feedback[x % this.feedback.length]
     }
 }
